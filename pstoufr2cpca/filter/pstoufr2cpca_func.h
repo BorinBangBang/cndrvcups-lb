8a6d8a27af531311de36b5541afb5bbdec07a122
Karl-Philipp Richter <krichter722@aol.de>
2015-01-22 11:34:39 +0100

    switched completely to autoconf and allowed unit tests with `check`

---
+++ pstoufr2cpca/filter/pstoufr2cpca_func.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   pstoufr2cpca.h
+ * Author: richter
+ *
+ * Created on 22. Januar 2015, 07:33
+ */
+
+#ifndef PSTOUFR2CPCA_H
+#define	PSTOUFR2CPCA_H
+
+int pstoufr2cpca_main(int argc, char *argv[]);
+
+#endif	/* PSTOUFR2CPCA_H */
+
---
+++ tests/pstoufr2cpca/filter/pstoufr2cpca_func_test.c
@@ -0,0 +1,67 @@
+/* 
+ * File:   pstoufr2cpca_test.c
+ * Author: richter
+ *
+ * Created on 22. Januar 2015, 07:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include "../../../pstoufr2cpca/filter/pstoufr2cpca_func.h"
+
+
+void
+setup (void)
+{
+  
+}
+
+void
+teardown (void)
+{
+  
+}
+
+START_TEST (test_pstoufr2cpca_main)
+{
+    char *argv[5];
+    // parameters according to `man filter`
+    argv[0] = "1"; //order
+    argv[1] = "richter"; //user
+    argv[2] = "title"; //title
+    argv[3] = "1"; //# of copies
+    argv[4] = "/home/richter/.bashrc"; //file name
+    int argc = 5;
+    
+    int returncode = pstoufr2cpca_main(argc, argv);
+    
+  ck_assert_int_eq (returncode, 0);
+}
+END_TEST
+
+Suite *
+money_suite (void)
+{
+  Suite *s = suite_create ("test_pstoufr2cpca_main");
+
+  /* Core test case */
+  TCase *tc_core = tcase_create ("test_pstoufr2cpca");
+  tcase_add_checked_fixture (tc_core, setup, teardown);
+  tcase_add_test (tc_core, test_pstoufr2cpca_main);
+  suite_add_tcase (s, tc_core);
+
+  return s;
+}
+
+int
+main (void)
+{
+  int number_failed;
+  Suite *s = money_suite ();
+  SRunner *sr = srunner_create (s);
+  srunner_run_all (sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed (sr);
+  srunner_free (sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
